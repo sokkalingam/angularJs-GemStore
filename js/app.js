bac6721ed45786531f689771fa915450f51cfca3
Sokkalingam SUBRAMANIAN <sokkalingam.subramanian@amadeus.com>
2016-03-17 23:40:05 -0400

    some bootstrap stuff

--- js/app.js
+++ js/app.js
@@ -16,6 +16,7 @@ app.controller('PanelController', function(){
 	};
 });
 
+
 // Test Data, later will come from backend service
 var gems = [
 		{
@@ -24,7 +25,9 @@ var gems = [
 			description : 'Shine bright like an American Diamond. Value for money, the looks that has no match.',
 			url : "http://www.gemandmineralsociety.org/gem_png_by_doloresdevelde-d57p0sp[1].png",
 			canPurchase : true,
-			soldOut : false
+			soldOut : false,
+			specifications: "",
+			reviews : []
 		},
 		{
 			name : 'African Diamond',
@@ -32,7 +35,9 @@ var gems = [
 			description : 'Shine bright like an African Diamond. Diamond as the way they were born',
 			url : "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR6fQe9z0ySHTIX8WzgucnSOwKqn7EuPhqzoTuYAu-5XMz-fY2H",
 			canPurchase : true,
-			soldOut : false
+			soldOut : false,
+			specifications: "",
+			reviews : []
 		},
 		{
 			name : 'Belgium Diamond',
@@ -40,5 +45,7 @@ var gems = [
 			description : 'Shine bright like an Belgium Diamond. Diamond the Belgium way',
 			url : "http://www.belgium-diamonds.com/images/shape_large_princess.jpg",
 			canPurchase : true,
-			soldOut : false
+			soldOut : false,
+			specifications: "",
+			reviews : []
 		}];
