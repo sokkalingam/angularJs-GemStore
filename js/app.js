fa991917bbf499a42a2ac565dd34129098eb02bf
Sokkalingam SUBRAMANIAN <sokkalingam.subramanian@amadeus.com>
2016-03-17 21:29:57 -0400

    use of ng-show and ng-hide if canPurchase and not soldOut

--- js/app.js
+++ js/app.js
@@ -9,5 +9,7 @@ var gem = {
 		 name: 'American Diamond',
 		 price: 89.95,
 		 description: 'Shine bright like an American Diamond. Value for money, the looks that has no match.',
-		 url: "http://www.gemandmineralsociety.org/gem_png_by_doloresdevelde-d57p0sp[1].png"
+		 url: "http://www.gemandmineralsociety.org/gem_png_by_doloresdevelde-d57p0sp[1].png",
+		 canPurchase: true,
+		 soldOut: false
 		};
