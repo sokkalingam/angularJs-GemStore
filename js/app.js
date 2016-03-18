8c6a029da23d2a5c18e3b364c53f642cbe1f2475
Sokkalingam SUBRAMANIAN <sokkalingam.subramanian@amadeus.com>
2016-03-17 21:23:55 -0400

    Displaying details of Gem with image

--- js/app.js
+++ js/app.js
@@ -8,5 +8,6 @@ app.controller('StoreController', function() {
 var gem = {
 		 name: 'American Diamond',
 		 price: 89.95,
-		 description: 'This is a description showing ',
+		 description: 'Shine bright like an American Diamond. Value for money, the looks that has no match.',
+		 url: "http://www.gemandmineralsociety.org/gem_png_by_doloresdevelde-d57p0sp[1].png"
 		};
