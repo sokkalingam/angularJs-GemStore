498e414e4e21ecc235f53b259ec3526513db9ab5
Sokkalingam SUBRAMANIAN <sokkalingam.subramanian@amadeus.com>
2016-04-16 11:46:04 -0400

    Problem: success messages are displayed for all gems

--- js/app/app.js
+++ js/app/app.js
@@ -8,7 +8,7 @@ app.controller('ReviewController', ['$scope', '$http', function($scope, $http) {
 	$scope.rating = 0;
 
 	$scope.getReviews = function(product) {
-		if (product.gemReview != null) {
+		if (product != null && product.gemReview != null) {
 			$scope.reviews = product.gemReview.reviews;
 			$scope.averageReview = product.gemReview.averageReview;
 		}
@@ -63,7 +63,7 @@ app.controller('TabController', ['$scope', function($scope) {
 app.controller('GemsController', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
 	
 	$scope.products = [];
-	$scope.product = { reviews:[] };
+	$scope.thisProduct = {};
 	$scope.sortByOptions = ['Price (low to high)', 'Price (high to low)', 'Average Review (low to high)', 'Average Review (high to low)'];
 	$scope.query = {name:'', minPrice:'', maxPrice:'', rating:''}
 
@@ -92,8 +92,8 @@ app.controller('GemsController', ['$scope', '$http', '$timeout', function($scope
 	};
 
 	$scope.addProduct = function() {
-		$http.post(baseUrl + '/gems', $scope.product).success(function(data) {
-			$scope.product = { reviews: [] };
+		$http.post(baseUrl + '/gems', $scope.thisProduct).success(function(data) {
+			$scope.thisProduct = {};
 			$scope.addProductForm.$setPristine();
 			$scope.addProductForm.$setUntouched();
 		});
@@ -112,22 +112,24 @@ app.controller('GemsController', ['$scope', '$http', '$timeout', function($scope
 	};
 
 	$scope.addToCart = function(product) {
-		$http.get(baseUrl + '/gems/' + product.id + '/addToCart').success(function(data) {
+		$http.post(baseUrl + '/gems/' + product.id + '/addToCart').success(function(data) {
 			$scope.getByQuery($scope.query);
 		});
 	};
 
 	$scope.checkout = function(product) {
-		product.quantity = product.quantity - 1;
-		$scope.updateProduct(product);
-		$scope.displayMessage(product);
+		$http.post(baseUrl + '/gems/' + product.id + '/checkout').success(function(data) {
+			$scope.getByQuery($scope.query);
+			$scope.displayMessage(product);
+		});
 	};
 
 	$scope.displayMessage = function(product) {
-		$scope.showMessage = true;
+		$scope.thisProduct = product;
+		$scope.thisProduct['showMessage'] = true;
 		$timeout(function() {
-			$scope.showMessage = false;
-		}, 3000);
+			$scope.thisProduct.showMessage = false;
+		}, 10000);
 	};
 
 }]);
@@ -143,9 +145,23 @@ app.controller('ShoppingcartController', ['$scope', '$rootScope', '$http', '$tim
 	}
 
 	$scope.removeFromCart = function(product) {
-		$http.get(baseUrl + '/gems/' + product.id + '/removeFromCart').success(function(data) {
+		$http.post(baseUrl + '/gems/' + product.id + '/removeFromCart').success(function(data) {
 			$scope.getProductsInCart();
 		});
 	};
+
+	// $scope.displayMessage = function() {
+	// 	$scope.showMessage = true;
+	// 	$timeout(function() {
+	// 		$scope.showMessage = false;
+	// 	}, 3000);
+	// };
+
+	// $scope.checkout = function(product) {
+	// 	$http.post(baseUrl + '/gems/' + product.id + '/checkout').success(function(data) {
+	// 		$scope.removeFromCart(product);
+	// 		$scope.displayMessage();
+	// 	});
+	// };
 
 }]);
