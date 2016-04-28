227a1ea42b54466623d9a335109d4748551be6c0
Sokkalingam SUBRAMANIAN <sokkalingam.subramanian@amadeus.com>
2016-04-28 13:56:09 -0400

    Letting Heroku to bind port

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var http = require("http");
-
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(5000, function(){
-    console.log('Server running on localhost:5000...');
+
+var port = process.env.PORT || 5000;
+
+connect().use(serveStatic(__dirname)).listen(port, function(){
+    console.log('Server running on PORT:' + port);
 });
